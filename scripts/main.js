52f281f99f795e3f8a3f5cae7506b92a3aec771c
Ilya Dedov <ilya-tor92@yandex.ru>
2021-05-22 20:18:22 +1000

    Проектная работа 4. Исправить семантику

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let openPopupBtn = document.querySelector('.profile__edit-button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let popup = document.querySelector('body > .popup');
-let closePopup = popup.querySelector('.popup__close-btn');
+let closePopupBtn = popup.querySelector('.popup__close-btn');
 let formElement = popup.querySelector('.form');
 let popupName = formElement.querySelector('.form__input_type_name');
 let popupAbout = formElement.querySelector('.form__input_type_about');
@@ -11,16 +11,16 @@ function openPopup() {
     popup.classList.add('popup_opened');
     popupName.value = profileTitle.textContent;
     popupAbout.value = profileSubtitle.textContent;
-};
-
-openPopupBtn.addEventListener('click', openPopup);
-closePopup.addEventListener('click', function () {
+}
+function closePopup() {
     popup.classList.remove('popup_opened');
-})
+}
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = popupName.value;
     profileSubtitle.textContent = popupAbout.value;
     popup.classList.remove('popup_opened');
 }
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
