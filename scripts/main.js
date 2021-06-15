bc520d9b2aa29501558c60bbd3e3f0695f7e64fd
Ilya Dedov <ilya-tor92@yandex.ru>
2021-06-16 02:13:26 +1000

    Проектная работа 6. Реализовать валидацию форм

--- scripts/main.js
+++ scripts/main.js
@@ -47,7 +47,6 @@ function createCard(name, link) {
     });
     return htmlElement;
 }
-
 function addCard(card) {
     listCards.prepend(card);
 }
@@ -57,20 +56,17 @@ function openPopup(modal) {
 function closePopup(modal) {
     modal.classList.remove('popup_opened');
 }
-
 function popupEditOpen(evt) {
     console.log(evt);
     editFormName.value = profileTitle.textContent;
     editFormAbout.value = profileSubtitle.textContent;
     openPopup(popupEdit);
 }
-
 function popupNewCardOpen(evt) {
     newCardFormName.value = '';
     NewCardFormLink.value = '';
     openPopup(popupNewCard);
 }
-
 function popupEditSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = editFormName.value;
@@ -85,6 +81,73 @@ function popupNewCardSubmitHandler(evt) {
     addCard(newCard);
     closePopup(popupNewCard);
 }
+
+function showInputError(formElement, inputElement, inputErrorClass, errorClass, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(errorClass);
+}
+
+function hideInputError(formElement, inputElement, inputErrorClass, errorClass) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(inputErrorClass);
+    errorElement.textContent = '';
+    errorElement.classList.remove(errorClass);
+}
+
+function checkInputValidity(formElement, inputElement, inputErrorClass, errorClass) {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputErrorClass, errorClass, inputElement.validationMessage);
+    }
+    else {
+        hideInputError(formElement, inputElement, inputErrorClass, errorClass);
+    }
+}
+function hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    })
+}
+
+function toggleButtonState(inputList, buttonElement, inactiveButtonClass) {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add(inactiveButtonClass);
+    }
+    else {
+        buttonElement.classList.remove(inactiveButtonClass);
+    }
+}
+
+function setEventListeners(formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass) {
+    const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+    const buttonElement = formElement.querySelector(submitButtonSelector);
+    toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement, inputErrorClass, errorClass);
+            toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+        })
+    })
+}
+
+function enableValidation(settings) {
+    const formList = document.querySelectorAll(settings.formSelector);
+    formList.forEach((formElement) => {
+        formElement.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+        })
+        setEventListeners(formElement, settings.inputSelector, settings.submitButtonSelector, settings.inactiveButtonClass, settings.inputErrorClass, settings.errorClass);
+    })
+}
+enableValidation({
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__save-btn',
+    inactiveButtonClass: 'form__save-btn_disabled',
+    inputErrorClass: 'form__input_type_error',
+    errorClass: 'form__input-error_visible'
+});
 
 popupEditForm.addEventListener('submit', popupEditSubmitHandler);
 popupNewCardForm.addEventListener('submit', popupNewCardSubmitHandler);
